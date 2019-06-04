2a0f22bd7940259f19ae990e0709642375510252
Dirt Deodara <thecrowleaps@gmail.com>
2019-06-03 19:53:08 -0700

    built out file structure. built header and profile classes

---
+++ src/home/index.js
@@ -0,0 +1,17 @@
+import { auth } from '../services/firebase.js';
+import App from './App.js';
+import '../utils/check-auth.js';
+
+auth.onAuthStateChanged(user => {
+    if(user) {
+        console.log('we have a user');
+    }
+    else {
+        console.log('no user');
+    }
+});
+
+
+const root = document.getElementById('app');
+const app = new App();
+root.appendChild(app.render());
---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+const config = {
+    apiKey: 'AIzaSyAhI_qOnJYfynspKI110G9053UOhgdDQnI',
+    authDomain: 'dirts-chat-room.firebaseapp.com',
+    databaseURL: 'https://dirts-chat-room.firebaseio.com',
+    projectId: 'dirts-chat-room',
+    storageBucket: 'dirts-chat-room.appspot.com',
+    messagingSenderId: '865812205856',
+    appId: '1:865812205856:web:a1e622b9ad92305b'
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+
+// export const db = firebase.database();
