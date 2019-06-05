0e003a8d36f0f81cfc4da3b62f810333ef057eb1
Dirt Deodara <thecrowleaps@gmail.com>
2019-06-04 20:45:49 -0700

    made the AddRoom component and rendered it in chatList

---
+++ src/home/AddRoom.js
@@ -0,0 +1,14 @@
+import Component from '../Component.js';
+
+class AddRoom extends Component {
+    renderTemplate() {
+        return /*html*/ `
+            <li class="chat-room">
+                <a href="">Guard Pullers</a>
+            </li>
+            
+    `;
+    }
+}
+
+export default AddRoom;
--- src/home/ChatList.js
+++ src/home/ChatList.js
@@ -1,14 +1,24 @@
 import Component from '../Component.js';
+import AddRoom from '../home/AddRoom.js';
 
 class ChatList extends Component {
     
+    render() {
+        const list = this.renderDOM();
+
+        const addRoom = new AddRoom();
+        const addRoomDOM = addRoom.render();
+        list.appendChild(addRoomDOM);
+
+        return list;
+    }
 
 
     renderTemplate() {
         return /*html*/ `
-        <ul id="chat-list">
-            <p>remove this p tag</p>
-        </ul>
+            <ul id="chat-list">
+                <p>remove this p tag</p>
+            </ul>
     `;
     }
 }
