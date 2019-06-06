261e8394d2ac4257a11af75834bda0e653d716de
Dirt Deodara <thecrowleaps@gmail.com>
2019-06-06 16:14:01 -0700

    rendered my header into my chatapp

--- src/chat/ChatApp.js
+++ src/chat/ChatApp.js
@@ -1,9 +1,15 @@
 import Component from '../Component.js';
+import Header from '../shared/Header.js';
 
 class ChatApp extends Component {
     render() {
         const dom = this.renderDOM();
         const main = dom.querySelector('main');
+
+        const header = new Header();
+        dom.insertBefore(header.render(), main);
+
+        return dom;
     }
 
     renderTemplate() {
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -13,5 +13,7 @@ export const app = firebase.initializeApp(config);
 export const auth = firebase.auth();
 
 export const db = firebase.database();
+
 export const dirtsRoomsRef = db.ref('rooms');
+
 window.db = db;
