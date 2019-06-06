5e283dd968f63ea6546f697ddb510c174f84e4bd
Dirt Deodara <thecrowleaps@gmail.com>
2019-06-06 08:31:02 -0700

    one last commit

--- src/home/index.js
+++ src/home/index.js
@@ -1,15 +1,6 @@
-import { auth } from '../services/firebase.js';
 import App from './App.js';
 import '../utils/check-auth.js';
 
-auth.onAuthStateChanged(user => {
-    if(user) {
-        // console.log(user, 'we have a user');
-    }
-    else {
-        // console.log('no user');
-    }
-});
 
 
 const root = document.getElementById('app');
